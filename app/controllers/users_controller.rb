3195951de8ea6ac75219cb6bc4f05d5994c6a723
rahul <rahulsingh2240@gmail.com>
2017-06-30 22:08:56 +0530

    signup finish

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = 'Welcome! To Pithugram'
+      flash[:success] = 'Welcome To Pithugram!'
       redirect_to @user
     else
       render 'new'
