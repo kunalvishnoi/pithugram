9f50ea5862bdb60f53ba0f357dec15028bdc6523
rahul <rahulsingh2240@gmail.com>
2017-07-02 14:46:26 +0530

    showing-microposts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170701110557) do
+ActiveRecord::Schema.define(version: 20170702075402) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["created_at", "user_id"], name: "index_microposts_on_created_at_and_user_id"
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
