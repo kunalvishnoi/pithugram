85b3f058b4b8397a711a8f6cbb2a5c16df67732a
rahul <rahulsingh2240@gmail.com>
2017-07-04 13:50:46 +0530

    Completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,7 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
                                        :following, :followers]
- .
-    before_action :correct_user,   only: [:edit, :update]
+   before_action :correct_user,   only: [:edit, :update]
     before_action :admin_user,     only: :destroy
 
   def new
@@ -66,7 +65,7 @@ class UsersController < ApplicationController
     @users = @user.followers.paginate(page: params[:page])
     render 'show_follow'
   end
-  
+
   private
 
   def user_params
