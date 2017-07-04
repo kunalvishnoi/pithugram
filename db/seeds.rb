209505f9d39bc0c88baf12c1d794691fff14ffeb
rahul <rahulsingh2240@gmail.com>
2017-07-04 13:56:35 +0530

    Completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,5 @@
-User.create!(name:  "rahul",
+User.create!(name:  "Rahul",
              email: "rahul@gmail.com",
              password:              "rahul123",
              password_confirmation: "rahul123",
              admin: true)
-
-99.times do |n|
-               name  = Faker::Name.name
-               email = "example-#{n+1}@railstutorial.org"
-               password = "password"
-               User.create!(name:  name,
-                            email: email,
-                            password:              password,
-                            password_confirmation: password)
-end
-
-users = User.order(:created_at).take(6)
-50.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.microposts.create!(content: content) }
-end
